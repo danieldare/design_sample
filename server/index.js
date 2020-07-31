5953cf286f5e19160e39a303bbc052346e2e7226
Oluwadare Daniel Segun <dodanieloluwadare@gmail.com>
2020-07-31 22:50:13 +0100

    change start command

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
 const path = require('path');
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5008;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(path.join(__dirname, '../build')));
-
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/ping', function (req, res) {
