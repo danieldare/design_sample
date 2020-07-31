c34f45b31f0329528db7b4595581a1cb45feb5c2
Oluwadare Daniel Segun <dodanieloluwadare@gmail.com>
2020-07-31 22:22:55 +0100

    remove favicon

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const favicon = require('express-favicon');
+
 const path = require('path');
+const port = process.env.PORT || 5005;
 const app = express();
 
-app.use(favicon(path.join(__dirname, '../build', 'favicon.ico')));
+// the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res) {
-  return res.send('Welcome to Gidara');
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
 });
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
-
-const port = process.env.PORT || 5005;
 
 app.listen(port, () => console.log(`app is running on port ${port}`));
